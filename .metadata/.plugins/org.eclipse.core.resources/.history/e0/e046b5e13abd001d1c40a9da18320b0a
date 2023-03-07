f49a8d51cbf33c670a1c82677406cf38753a2c25
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2023-03-07 23:55:27 +0100

    clases añadidas4

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e0/e046b5e13abd001d1c40a9da18320b0a
@@ -0,0 +1,10 @@
+package testing;
+
+public abstract class TestingDepartamentos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- GRUPO17_actividad7/src/testing/TestingDepartamentos.java
+++ GRUPO17_actividad7/src/testing/TestingDepartamentos.java
@@ -1,9 +1,15 @@
 package testing;
 
+import daos.DepartamentoDao;
+import daos.DepartamentoDaoImplMy8;
+
 public abstract class TestingDepartamentos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DepartamentoDao dDao = new DepartamentoDaoImplMy8();
+		
+		System.out.println(dDao.buscarTodos());
 
 	}
 
